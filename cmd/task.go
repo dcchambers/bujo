b69e6e9d4dd69d28633d38113d0a3c933bed310d
Dakota Chambers <dakotachambers@gmail.com>
2021-10-30 13:46:09 -0500

    Scaffold task subcommand

---
+++ cmd/task.go
@@ -0,0 +1,41 @@
+package cmd
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+// taskCmd represents the task command
+var taskCmd = &cobra.Command{
+	Use:   "task",
+	Short: "A brief description of your command",
+	Long: `A longer description that spans multiple lines and likely contains examples
+and usage of using your command. For example:
+
+Cobra is a CLI library for Go that empowers applications.
+This application is a tool to generate the needed files
+to quickly create a Cobra application.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		reader := bufio.NewReader(os.Stdin)
+		fmt.Printf("Enter Task: ")
+		task, _ := reader.ReadString('\n')
+		fmt.Printf(" ○ %s", task)
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(taskCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// taskCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// taskCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
